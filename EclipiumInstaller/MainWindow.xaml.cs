ee6b84618108e372630b2d81804f6e8439960948
TheTrioGames <66476312+TheTrioGames@users.noreply.github.com>
2020-11-18 11:48:58 +0100

    1.0

--- EclipiumInstaller/MainWindow.xaml.cs
+++ EclipiumInstaller/MainWindow.xaml.cs
@@ -10,25 +10,31 @@ using System.IO.Compression;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using IWshRuntimeLibrary;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
 
 namespace EclipiumInstaller
 {
-    
+
     public partial class MainWindow : Window
     {
-        public string ApplicationName { get; set; }
-        public string ProgressMessage { get; set; }
-        public string ApplicationUrl { get; set; }
+        public string ApplicationName;
+        public string ProgressMessage;
+        public string ApplicationUrl;
         private string TempDir;
         private Button btn;
-        public int Progress { get; set; }
+        public int Progress;
+        private string a = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+        private Foo f;
+
         public MainWindow()
         {
             InitializeComponent();
-            InitializeInstaller("{AppName}", "{PackageUrl}");
+            InitializeInstaller("{AppName}", "{PackageUrl}", "{ImageUrl}");
+            //InitializeInstaller("Hieroctive", "https://launcher.eclipium.ga/GamesPackages/Hieroctive.zip", "https://launcher.eclipium.ga/Images/hieroctiveRounded.png");
         }
 
-        private void InitializeInstaller(string AppName,string AppUrl) 
+        private void InitializeInstaller(string AppName, string AppUrl, string ImageUrl)
         {
             ApplicationName = AppName;
             title.Content = ApplicationName;
@@ -36,13 +42,14 @@ namespace EclipiumInstaller
             ProgressMessage = "En attente";
             txt.Content = ProgressMessage;
             ApplicationUrl = AppUrl;
+            Logo.Source = new BitmapImage(new Uri(ImageUrl));
         }
 
-        Stream GetStreamFromFile(string filename)
+        private static Stream GetStreamFromFile(string filename)
         {
             var assembly = typeof(MainWindow).GetTypeInfo().Assembly;
 
-            var stream = assembly.GetManifestResourceStream("EclipiumInstalller." + filename);
+            var stream = assembly.GetManifestResourceStream("EclipiumInstaller." + filename);
 
             return stream;
         }
@@ -54,18 +61,18 @@ namespace EclipiumInstaller
 
         private void btnDownload_Click(object sender, RoutedEventArgs e)
         {
-             ((Button)sender).IsEnabled = false;
-             btn = ((Button)sender);
+            ((Button)sender).IsEnabled = false;
+            btn = ((Button)sender);
             string result = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-             Directory.CreateDirectory(result + "\\TempEclipium");
-             TempDir = result + "\\TempEclipium\\";
-            
-             ProgressMessage = "Téléchargement";
-             txt.Content = ProgressMessage;
-             WebClient webClient = new WebClient();
-             webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
-             webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
-             webClient.DownloadFileAsync(new Uri(ApplicationUrl), TempDir+"arch.zip");
+            Directory.CreateDirectory(result + "\\TempEclipium");
+            TempDir = result + "\\TempEclipium\\";
+
+            ProgressMessage = "Téléchargement";
+            txt.Content = ProgressMessage;
+            WebClient webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            webClient.DownloadFileAsync(new Uri(ApplicationUrl), TempDir + "arch.zip");
         }
 
         private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
@@ -77,22 +84,26 @@ namespace EclipiumInstaller
         private async void Completed(object sender, AsyncCompletedEventArgs e)
         {
             bool b = true;
-                ProgressMessage = "Extraction";
-                txt.Content = ProgressMessage;
-                await Task.Delay(1000);
+            ProgressMessage = "Extraction";
+            txt.Content = ProgressMessage;
+            await Task.Delay(1000);
             try
             {
                 ZipFile.ExtractToDirectory(TempDir + "arch.zip", TempDir);
-                
+
             }
-            catch
+            catch(FileNotFoundException)
+            {
+                b = false;
+            }
+            catch(InvalidDataException)
             {
                 b = false;
             }
             if(b)
-            { 
+            {
                 string js = System.IO.File.ReadAllText(TempDir + "package.json");
-                Foo f = JsonConvert.DeserializeObject<Foo>(js);
+                f = JsonConvert.DeserializeObject<Foo>(js);
                 Progress = 150;
                 pb.Value = Progress;
                 ProgressMessage = "Installation";
@@ -100,13 +111,13 @@ namespace EclipiumInstaller
                 await Task.Delay(1000);
 
 
-                var a = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+
                 Directory.CreateDirectory(a + "\\Eclipium");
                 DirectoryInfo dir = new DirectoryInfo(a + "\\Eclipium\\" + f.Name);
 
                 if(dir.Exists)
                 {
-                    
+
                     foreach(FileInfo file in dir.GetFiles())
                     {
                         file.Delete();
@@ -116,10 +127,10 @@ namespace EclipiumInstaller
                         dirt.Delete(true);
                     }
                 }
-                DirectoryCopy(TempDir + "bin", a + "\\Eclipium\\" + f.Name,true);
+                DirectoryCopy(TempDir + "bin", a + "\\Eclipium\\" + f.Name, true);
                 var remover = GetStreamFromFile("Remove.exe");
-                SaveStreamAsFile(a + "\\Eclipium\\" + f.Name, remover, "Remove.exe");
-                
+                SaveStreamAsFile(a + "\\Eclipium\\" + f.Name + "\\" + "Remove.exe", remover);
+
                 System.IO.File.Move(TempDir + "package.json", a + "\\Eclipium\\" + f.Name + "\\package.json");
                 System.IO.DirectoryInfo di = new DirectoryInfo(TempDir);
 
@@ -134,7 +145,8 @@ namespace EclipiumInstaller
                 Directory.Delete(TempDir);
                 CreateShortcut(f.Name, Environment.GetFolderPath(Environment.SpecialFolder.Desktop), a + "\\Eclipium\\" + f.Name + "\\" + f.MainExe);
                 CreateShortcut(f.Name, Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), a + "\\Eclipium\\" + f.Name + "\\" + f.MainExe);
-                CreateShortcut(f.Name + " Uninstaller" , Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), a + "\\Eclipium\\" + f.Name + "\\" + "Remove.exe");
+                CreateShortcut(f.Name + " Uninstaller", Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), a + "\\Eclipium\\" + f.Name + "\\" + "Remove.exe");
+                UninstallRegistery();
                 Progress = 200;
                 pb.Value = Progress;
                 ProgressMessage = "Installation terminée";
@@ -144,46 +156,50 @@ namespace EclipiumInstaller
             }
             else
             {
-                   try
-                      {
+                try
+                {
                     System.IO.File.Delete(TempDir + "arch.7z");
 
-                      }
-                      catch { }
-                      try
-                      { Directory.Delete(TempDir); }
-                      catch { }
-                      Progress = 0;
-                      pb.Value = Progress;
-                      ProgressMessage = "Pas d'internet, veuillez réessayer plus tard";
-                      txt.Content = ProgressMessage;
-                      btn.IsEnabled = true;
+                }
+                catch { }
+                try
+                { Directory.Delete(TempDir); }
+                catch { }
+                Progress = 0;
+                pb.Value = Progress;
+                ProgressMessage = "Pas d'internet, veuillez réessayer plus tard";
+                txt.Content = ProgressMessage;
+                btn.IsEnabled = true;
             }
         }
 
-        public static void SaveStreamAsFile(string filePath, Stream inputStream, string fileName)
+        private void UninstallRegistery()
         {
-            DirectoryInfo info = new DirectoryInfo(filePath);
-            if(!info.Exists)
-            {
-                info.Create();
-            }
+            RegistryKey UninstallKey = Registry.LocalMachine.OpenSubKey("SOFTWARE", true).OpenSubKey("Microsoft", true)
+                .OpenSubKey("Windows", true).OpenSubKey("CurrentVersion", true).OpenSubKey("Uninstall", true)
+                .CreateSubKey(ApplicationName, true);
+            UninstallKey.SetValue("DisplayIcon", a + "\\Eclipium\\" + f.Name + "\\" + f.MainExe, RegistryValueKind.String);
+            UninstallKey.SetValue("DisplayName", ApplicationName, RegistryValueKind.String);
+            UninstallKey.SetValue("DisplayVersion", f.VersionName, RegistryValueKind.String);
+            UninstallKey.SetValue("Publisher", "Eclipium", RegistryValueKind.String);
+            UninstallKey.SetValue("UninstallPath", a + "\\Eclipium\\" + f.Name + "\\" + "Remove.exe", RegistryValueKind.String);
+            UninstallKey.SetValue("UninstallString", a + "\\Eclipium\\" + f.Name + "\\" + "Remove.exe", RegistryValueKind.String);
+            UninstallKey.SetValue("InstallLocation", a + "\\Eclipium\\" + f.Name, RegistryValueKind.String);
+            UninstallKey.SetValue("NoModify", 1, RegistryValueKind.DWord);
+            UninstallKey.SetValue("NoRepair", 1, RegistryValueKind.DWord);
+        }
 
-            string path = Path.Combine(filePath, fileName);
+        public static void SaveStreamAsFile(string filePath, Stream inputStream)
+        {
+
+            string path = filePath;
             using(FileStream outputFileStream = new FileStream(path, FileMode.Create))
             {
                 inputStream.CopyTo(outputFileStream);
             }
         }
 
-        private class Foo
-        {
-            public string Name { get;  set; }
-            public string MainExe { get; set; }
-            public string VersionName { get; set; }
-            public int VersionCode { get; set; }
-            public string Date { get; set; }
-        }
+
 
 
         public static void CreateShortcut(string shortcutName, string shortcutPath, string targetFileLocation)
@@ -191,8 +207,8 @@ namespace EclipiumInstaller
             string shortcutLocation = System.IO.Path.Combine(shortcutPath, shortcutName + ".lnk");
             WshShell shell = new WshShell();
             IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
-            shortcut.TargetPath = targetFileLocation;                 // The path of the file that will launch when the shortcut is run
-            shortcut.Save();                                    // Save the shortcut
+            shortcut.TargetPath = targetFileLocation;
+            shortcut.Save();
         }
 
         private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
@@ -230,6 +246,15 @@ namespace EclipiumInstaller
                 }
             }
         }
+    }
+
+    class Foo
+    {
+        public string Name { get; set; }
+        public string MainExe { get; set; }
+        public string VersionName { get; set; }
+        public int VersionCode { get; set; }
+        public string Date { get; set; }
     }
 
 
